99f136b8782d0db0ce2fc36c9f9397fd73b96d9e
Santhiya148 <santhiyaeswari16@gmail.com>
2026-02-15 12:20:30 +0530

    Add files via upload

---
+++ loop.py
@@ -0,0 +1,44 @@
+name=input("Enter Student Name:")
+regno=input("Enter Register Number:")
+ 
+
+n=int(input("Enter number of subjects:"))
+
+
+total = 0
+result ="Pass"
+
+
+for i in range(1 ,n+1):
+    mark =int(input("Enter mark for subject {i}:"))
+
+    if mark<35:
+        result="Fail"
+        
+    total +=mark
+
+    average = total/n
+
+    if result == "Fail":
+        grade = "No Grade"
+
+    elif average >= 90:
+        grade ="A"
+
+    elif average >=75:
+        grade ="B"
+
+    elif average >=60:
+        grade ="C"
+
+    else:
+        grade ="D"
+
+print("\n-------Student Mark STATEMENT --------")
+
+print("Name :",name)
+print("Register No :",regno)
+print("Total MArks :",total)
+print("Average :",average)
+print("Result :",result)
+print("Grade :",grade)
